61f18d31134d9d9fbe00ff77cc0575cabebbd0a4
lighthouse-dev <lighthouse2.dev@gmail.com>
2019-11-30 19:38:57 +0900

    :+1: ログイン機能実装

--- App.tsx
+++ App.tsx
@@ -1,24 +1,50 @@
+import React from 'react';
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
 import LoginScreen from './src/screens/LoginScreen';
+import TodoListScreen from './src/screens/TodoListScreen';
+import ENV from './env.json';
+
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENV.FIREBASE_APPID,
+  measurementId: ENV.FIREBASE_MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig);
 
 const App = createStackNavigator(
   {
-    LoginScreen: { screen: LoginScreen }
+    LoginScreen: { screen: LoginScreen },
+    TodoListScreen: { screen: TodoListScreen }
   },
   {
     defaultNavigationOptions: {
       headerTitle: 'Polaris',
       headerStyle: {
-        backgroundColor: '#3F52B5',
+        backgroundColor: '#ffffff',
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.2,
-        shadowRadius: 2
+        shadowRadius: 2,
+        ...Platform.select({
+          ios: {},
+          android: {
+            height: 60
+          }
+        })
       },
       headerTitleStyle: {
-        color: '#FFF',
+        color: '#333333',
         fontSize: 20,
         fontWeight: 'bold'
       },
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,112 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity
+} from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import firebase from 'firebase';
+import { StackActions, NavigationActions } from 'react-navigation';
+import Loading from '../elements/Loading';
 
 class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isLoading: true
+  };
+
+  async componentDidMount() {
+    // 画面がレンダリングされた後に実行する
+    const email = await SecureStore.getItemAsync('email');
+    const password = await SecureStore.getItemAsync('password');
+
+    if (email == null || password == null) {
+      this.setState({ isLoading: false });
+    }
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ isLoading: false });
+        this.navigateToHome();
+      })
+      .catch(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+  navigateToHome() {
+    const resetAction = StackActions.reset({
+      index: 0,
+      actions: [NavigationActions.navigate({ routeName: 'TodoListScreen' })]
+    });
+
+    this.props.navigation.dispatch(resetAction);
+  }
+
+  handleSubmit() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        SecureStore.setItemAsync('email', this.state.email);
+        SecureStore.setItemAsync('password', this.state.password);
+        this.navigateToHome();
+      })
+      .catch(() => {});
+  }
+
+  handlePress() {
+    this.props.navigation.navigate('SignUpScreen');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text> ログイン </Text>
+        <Loading text="Loading..." isLoading={this.state.isLoading} />
+        <Text style={styles.title}> ログイン </Text>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={text => {
+            this.setState({ email: text });
+          }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+          underlineColorAndroid="transparent"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={text => {
+            this.setState({ password: text });
+          }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+          secureTextEntry
+          underlineColorAndroid="transparent"
+        />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="#c93467"
+        >
+          <Text style={styles.buttonTitle}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableOpacity
+          style={styles.signup}
+          onPress={this.handlePress.bind(this)}
+        >
+          <Text style={styles.signupText}> ➕ Sign up </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -17,6 +118,41 @@ const styles = StyleSheet.create({
     width: '100%',
     padding: 24,
     backgroundColor: '#FFF'
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#EEE',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    padding: 8
+  },
+  button: {
+    backgroundColor: '#FF4181',
+    height: 48,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '70%'
+  },
+  buttonTitle: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  signup: {
+    alignSelf: 'center',
+    marginTop: 20
+  },
+  signupText: {
+    fontSize: 16
   }
 });
 
