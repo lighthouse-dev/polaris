b57a51012393aa977117be087035413dc669f23f
lighthouse-dev <lighthouse2.dev@gmail.com>
2020-03-28 12:05:09 +0900

    View -> Layoutに修正

--- src/elements/CardItem.tsx
+++ src/elements/CardItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleProp, ViewStyle, View, TouchableHighlight } from 'react-native';
-import { CardHeader, Card, Text } from '@ui-kitten/components';
+import { StyleProp, ViewStyle, TouchableHighlight } from 'react-native';
+import { CardHeader, Card, Text, Layout } from '@ui-kitten/components';
 import { Memo } from '../screens/memo/MemoListScreen';
 
 export interface Props {
@@ -16,11 +16,11 @@ const CardItem = (props: Props) => {
   return (
     <Card style={style} header={Header} key={memo.index}>
       <TouchableHighlight onPress={onPress} underlayColor="#FFF">
-        <View>
+        <Layout>
           <Text>{memo.content.substring(0, 120)} ...</Text>
           {/* TODO: Tag表示できるように対応 */}
           <Text>{memo.tag}</Text>
-        </View>
+        </Layout>
       </TouchableHighlight>
     </Card>
   );
